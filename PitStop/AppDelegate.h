75727de2521c7c4f2b0cdd6858b35d487cec5576
Hriday Kemburu <hridaykemburu@Hridays-MacBook-Pro.local>
2014-09-02 10:28:36 -0700

    Initial Commit

---
+++ PitStop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PitStop
+//
+//  Created by Hriday Kemburu on 9/2/14.
+//  Copyright (c) 2014 Hriday Kemburu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PitStop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PitStop
+//
+//  Created by Hriday Kemburu on 9/2/14.
+//  Copyright (c) 2014 Hriday Kemburu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
